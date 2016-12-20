e3e010d3ec2e7dcb00ff88a3f6b52c722e439aa5
Dmitry Chopey <dmitry.chopey@gmail.com>
2016-12-20 17:23:01 +0200

    Add additional condition

--- lib/rake_scheduler.rb
+++ lib/rake_scheduler.rb
@@ -3,29 +3,31 @@ require 'rake'
 
 module RakeScheduler
   class << self
-    def daily(task_name)
-      invoke_task(task_name)
+    def daily(task_name, &block)
+      invoke_task(task_name, &block)
     end
 
-    def weekly(task_name)
-      invoke_task(task_name, weekly_condition)
+    def weekly(task_name, &block)
+      invoke_task(task_name, weekly_condition, &block)
     end
 
-    def monthly(task_name)
-      invoke_task(task_name, monthly_condition)
+    def monthly(task_name, &block)
+      invoke_task(task_name, monthly_condition, &block)
     end
 
-    def yearly(task_name)
-      invoke_task(task_name, yearly_condition)
+    def yearly(task_name, &block)
+      invoke_task(task_name, yearly_condition, &block)
     end
 
     private
-
+    
     def invoke_task(task_name, invoke = true)
       begin
-        Rake::Task[task_name].invoke and return true if invoke
+        additional_condition = true
+        additional_condition = yield Date.today if block_given?
+        Rake::Task[task_name].invoke and return true if invoke && additional_condition
         false
-      rescue Exception
+      rescue RuntimeError
         "Don't know how to build task '#{task_name}'"
       end
     end
